41e2ff4d7cd81e886449c13d4affe147bd0054e8
Namitha0505 <76142177+Namitha0505@users.noreply.github.com>
2021-02-04 23:28:42 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,1 +1,30 @@
-//WAP to find the volume of a tromboloid using 4 functions.
+#include<stdio.h>
+int inp(int,int,int);
+int out(int);
+int res(int,int,int);
+int main()
+{
+int a,h,b,d,vol,q,p;
+a=inp(h,b,d);
+printf(“enter h value”);
+scanf(“%d”,&h);
+printf(“enter b value”);
+scanf(“%d”,&b);
+printf(“enter d value”);
+scanf(“%d”,&d);
+vol=res(h,b,d);
+q=out(p);
+printf(“%d is the volume of tromboloid”,vol);
+}
+int inp(int x,int y,int z)
+{
+return (x,y,z);
+}
+int res(int h,int b,int d)
+{
+return ((h*b*d)+(d/b))/3;
+}
+int out(s)
+{
+return 0;
+}
