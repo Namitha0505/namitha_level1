6a905c021bfdc63c896fba8609109196f5cf0203
Namitha0505 <76142177+Namitha0505@users.noreply.github.com>
2021-02-15 14:40:55 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -1,14 +1,34 @@
 #include<stdio.h>
+float inp();
+float out(float);
+float res(float,float,float);
 int main()
 {
-float h,b,d,volume;
-printf(“enter h value”);
-scanf(“%f”,&h);
-printf(“enter b value”);
-scanf(“%f”,&b);
-printf(“enter d value ”);
-scanf(“%f”,&d);
-volume=((h*d*b)+(d/b))/3;
-printf(“ volume of tromboloid is %f ”,volume);
+float a,e,c,p,h,b,d,q,v;
+printf(“enter the value of h”);
+a=inp();
+printf(“enter the value of d”);
+e=inp();
+printf(“enter the value of b”);
+c=inp();
+p=res(a,e,c);
+q=out(p);
+return 0;
+}
+float inp()
+{
+float x;
+scanf(“%f”,&x);
+return x;
+}
+float res(float h,float  b,float d)
+{
+float volume;
+volume=((h*b*d)+(d/b))/3;
+return volume;
+}
+float out(float volume)
+{
+printf(“volume of tromboloid is %f”,volume);
 return 0;
 }
